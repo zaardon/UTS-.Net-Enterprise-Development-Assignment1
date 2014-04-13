08ad1f0429b2e5e47a51bbf0737a7c51d3008fb3
Alex Smith <smith.alexanderjames@gmail.com>
2014-04-13 17:31:18 +1000

    Revert "yay more commits for datasets"
    This reverts commit 6810a134dfc99decf7c7e4cd5013ad26474a3b1d.

--- BlueConsultingManagementSystem/BlueConsultingManagementSystemLogic/DatabaseHandler.cs
+++ BlueConsultingManagementSystem/BlueConsultingManagementSystemLogic/DatabaseHandler.cs
@@ -3,17 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DataAccessSystemLogic;
-using DataAccessSystemLogic.ExpenseTableAdapters;
 
 namespace BlueConsultingManagementSystemLogic
 {
-    public class DatabaseHandler
+    class DatabaseHandler
     {
-        public Expense.ExpenseDBDataTable doThis()
-        {
-            ExpenseDBTableAdapter db = new ExpenseDBTableAdapter();
-            return db.StaffViewAll();
-        }
     }
 }
