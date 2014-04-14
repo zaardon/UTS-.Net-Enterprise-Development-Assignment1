eca63504e6684cfebb10fba854fa18da8e978cdd
Alex Smith <smith.alexanderjames@gmail.com>
2014-04-15 01:30:31 +1000

    Revert "We can now do datasets"
    This reverts commit 0059ca18c3965ccb7ffa876321c69e8ff85653eb.

--- BlueConsultingManagementSystem/BlueConsultingManagementSystemLogic/DatabaseHandler.cs
+++ BlueConsultingManagementSystem/BlueConsultingManagementSystemLogic/DatabaseHandler.cs
@@ -1,43 +1,12 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace BlueConsultingManagementSystemLogic
 {
-    public class DatabaseHandler
+    class DatabaseHandler
     {
-
-        public DatabaseHandler()
-        {
-            //string pathToData = ConfigurationManager.AppSettings["DataDirectoryLocation"];
-            //string dataDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, pathToData));
-            //AppDomain.CurrentDomain.SetData("DataDirectory", dataDirectory);
-        }
-
-        public DataSet doThis()
-        {
-
-            var connectionString = ConfigurationManager.ConnectionStrings["BlueConsultingDBString"].ConnectionString;
-            var connection = new SqlConnection(connectionString);
-            var selectCommand = new SqlCommand("SELECT DISTINCT ReportName, ConsultantName, StatusReport, Dept_type FROM ExpenseDB WHERE StatusReport = 'Approved'", connection);
-            var adapter = new SqlDataAdapter(selectCommand);
-
-            var resultSet = new DataSet();
-            adapter.Fill(resultSet);
-
-            connection.Close();
-
-            return resultSet;
-
-        }
-
     }
 }
--- BlueConsultingManagementSystem/BlueConsultingManagementSystemUI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlueConsultingManagementSystemUI.Global" Language="C#" %>
