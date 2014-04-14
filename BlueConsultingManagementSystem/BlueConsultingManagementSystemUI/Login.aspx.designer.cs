3f3d7a29dee616e3b9e1ffd57ab1d489d5b1c7ff
james chahwan <j.r.chahwan@gmail.com>
2014-04-15 00:22:56 +1000

    OH SHIT GOT REAL

--- BlueConsultingManagementSystem/BlueConsultingManagementSystemUI/Login.aspx.designer.cs
+++ BlueConsultingManagementSystem/BlueConsultingManagementSystemUI/Login.aspx.designer.cs
@@ -22,39 +22,12 @@ namespace BlueConsultingManagementSystemUI {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Login2 control.
+        /// Login1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Login Login2;
-        
-        /// <summary>
-        /// CreateUserWizard1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CreateUserWizard CreateUserWizard1;
-        
-        /// <summary>
-        /// CreateUserWizardStep1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CreateUserWizardStep CreateUserWizardStep1;
-        
-        /// <summary>
-        /// CompleteWizardStep1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CompleteWizardStep CompleteWizardStep1;
+        protected global::System.Web.UI.WebControls.Login Login1;
     }
 }
